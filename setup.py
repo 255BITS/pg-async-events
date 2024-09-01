d7055df2151d2c96d7be76f9cb966ec599acf4b5
Martyn Garcia <martyn.garcia@gmail.com>
2024-09-01 11:39:04 -0600

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pg-async-events',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'asyncpg>=0.24.0',
